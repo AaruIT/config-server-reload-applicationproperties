aa429cd93716ec45498d0c4b108750f2e08462c1
AaruIT <jesuisvenkat@gmail.com>
2025-02-07 21:08:37 -0700

    pushing local code to git hub repo

---
+++ src/main/java/in/ashokit/ConfigServerForRefreshScopeApplication.java
@@ -0,0 +1,13 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerForRefreshScopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerForRefreshScopeApplication.class, args);
+	}
+
+}
